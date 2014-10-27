81e1ae3e2aba09010520c884f866d9c1939b67f0
ravonyx <ravonyxa@gmail.com>
2014-10-27 22:46:42 +0100

    Makefile

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "libesgi/linked_list.h"
+void parser();
+
+int main(int argc, char** argv)
+{
+	(void) argc;
+	(void) argv;
+	t_maillon t;
+	t.data = 0;
+	return t.data;
+}
+
+
+void parser()
+{
+ 
+}
+
